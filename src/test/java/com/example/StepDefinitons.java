024f4e0fa792cf7ca20a5c0de40d384917b758d2
Saul_Amaris <saul.isusherrero@amaris.com>
2025-02-18 23:02:05 +0100

    test1

---
+++ src/test/java/com/example/StepDefinitons.java
@@ -0,0 +1,21 @@
+package com.example
+
+import io.cucumber.java.en.Given
+import io.cucumber.java.en.When
+import io.cucumber.java.en.Then
+
+public class StepDefinitons{
+    @Given("I have an example")
+    public void i_have_an_example(){
+        //Step implementation
+    }
+
+    @When("I run the example"){
+        //Step implementation
+    }
+
+    @Then("I should see the example run successfully")
+    public void i_see_the_example_run_successfully(){
+        //Step implementation
+    }
+}
